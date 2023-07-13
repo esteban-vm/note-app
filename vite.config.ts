e6083b3d24fb766b39b1461ed8a1ff3086d1e7f4
esteban-vm <estebanvm90@outlook.com>
2023-07-13 09:07:44 -0400

    refactors

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type CommonServerOptions } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import postCSSNesting from 'postcss-nesting'
 
+const commonOptions: CommonServerOptions = { open: true, host: true }
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
-  server: { open: true, host: true, port: 5_174 },
-  preview: { open: true, host: true, port: 5_175 },
+  server: { ...commonOptions, port: 5_174 },
+  preview: { ...commonOptions, port: 5_175 },
   css: { postcss: { plugins: [postCSSNesting()] } },
 })
